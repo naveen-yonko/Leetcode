683285a2f24a2bd84378d259a1cb726b0318d849
Naveen <naveenragavi5_bit27@mepcoeng.ac.in>
2026-02-13 00:07:29 +0530

    Time: 16 ms (4.82%) | Memory: 45.2 MB (6.45%) - LeetSync

---
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String replaceDigits(String s) {
+        StringBuilder sb = new StringBuilder();
+        int size = s.length();
+        for(int i=1;i<size;i=i+2){
+            
+            int c = (int) s.charAt(i-1);
+            int n = s.charAt(i)-'0';
+            sb.append(s.charAt(i-1));
+            sb.append((char)(c+n));
+            System.out.println(i+" "+c+" "+n+" "+(char)(c+n));
+        }
+        if(size%2 !=0){
+            sb.append(s.charAt(size-1));
+        }
+        return sb.toString();
+    }
+}
