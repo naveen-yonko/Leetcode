ddaa7d03de9c1548f395f8c84cf4487aba08ab94
Naveen <naveenragavi5_bit27@mepcoeng.ac.in>
2026-02-13 00:07:55 +0530

    Time: 1 ms (58.71%) | Memory: 42.6 MB (96.82%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
@@ -8,7 +8,7 @@ class Solution {
             int n = s.charAt(i)-'0';
             sb.append(s.charAt(i-1));
             sb.append((char)(c+n));
-            System.out.println(i+" "+c+" "+n+" "+(char)(c+n));
+            //System.out.println(i+" "+c+" "+n+" "+(char)(c+n));
         }
         if(size%2 !=0){
             sb.append(s.charAt(size-1));
