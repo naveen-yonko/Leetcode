cd8ac74591b7fdf8f147b6d8919b0320c8d2506f
Naveen <naveenragavi5_bit27@mepcoeng.ac.in>
2026-02-14 17:23:44 +0530

    Time: 20 ms (16.34%) | Memory: 44.2 MB (15.71%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> hm = new HashMap<>();
-        HashMap<Character,Character> hm1 = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(!hm.containsKey(s.charAt(i))){
-                hm.put(s.charAt(i),t.charAt(i));
-            }else{
-                if(hm.get(s.charAt(i)) != t.charAt(i)){return false;}
+        HashMap<Character, Character> hm = new HashMap<>();
+        HashMap<Character, Character> hm1 = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!hm.containsKey(s.charAt(i))) {
+                hm.put(s.charAt(i), t.charAt(i));
+            } else {
+                if (hm.get(s.charAt(i)) != t.charAt(i)) {
+                    return false;
+                }
             }
-            if(!hm1.containsKey(t.charAt(i))){
-                hm1.put(t.charAt(i),s.charAt(i));
-            }else{
-                if(hm1.get(t.charAt(i)) != s.charAt(i)){return false;}
+            if (!hm1.containsKey(t.charAt(i))) {
+                hm1.put(t.charAt(i), s.charAt(i));
+            } else {
+                if (hm1.get(t.charAt(i)) != s.charAt(i)) {
+                    return false;
+                }
             }
         }
         //System.out.println(hm);
