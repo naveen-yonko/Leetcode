833ca9817cc9367b329228ee683f3a5a16c7ce1a
Naveen <naveenragavi5_bit27@mepcoeng.ac.in>
2026-02-21 22:50:13 +0530

    Time: 17 ms (80.89%) | Memory: 50 MB (15.39%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,8 +1,10 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
-        int res=0;
-        for(int i=0;i<=nums.length-2;i+=2){res+=Math.min(nums[i],nums[i+1]);}
+        int res = 0;
+        for (int i = 0; i <= nums.length - 2; i += 2) {
+            res +=nums[i];
+        }
         return res;
     }
 }
